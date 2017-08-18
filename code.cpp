ede4427aed45b097b562201f79278a6e34539dd9
Kokoro <runnrairu@gmail.com>
2017-08-18 19:43:39 +0900

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -8,21 +8,50 @@ double sdlab_normal(double mu, double sigma);
 double sdlab_uniform();
 int main(){
   double F,G,C,D;
-  int n=1000;
+  int n=5+1;
    F=0.5;
   G=0.6;
   C=0.3;
    D=0.2;
-    double X[n],Y[n],W[n],B[n];
-　X[0]=0;
+    double X[n],Y[n],W[n],B[n],Xhat[n];
+    X[0]=0;
    Y[0]=0;
+    Xhat[0]=0;
+   int Im=pow(2,n)+1;
+int i;
+int j;
+int l;
 
-for(int i=0;i<=10000;i++){
-    for(int j=0;j<=n;j++){
-
-    
+for(i=0;i<=10;i++){
+    for(int j=1;j<=Im;j=j+2){
+         double a;
+         a=sdlab_normal(0,1);
+         double b;
+         b=sdlab_normal(0,1);
+         for(int l;l<=n;l++){
+         W[l]=W[l]+Haar(j,i,l)*a;
+         B[l]=B[l]+Haar(j,i,l)*b;
+         }  
     }
 }
+double deltat=1/n;
+for(int l=1;l<=n;l++){
+X[l+1]=X[l]+deltat*F*X[l]+C*(B[l+1]-B[l]);
+Y[l+1]=Y[l]+deltat*G*X[l]+D*(W[l+1]-W[l]);
+}
+for(l=1;l<n;l++){
+Xhat[l]=(F-(pow(G,2)*S[l]/pow(D,2)))*Xhat[l]*deltat+(G*S[l]/pow(D,2))*(Y[l+1]-Y[l]);
+}
+for(l=1;l<n;l++){
+print X[l];
+} 
+for(l=1;l<n;l++){
+print X[l];
+} 
+
+
+
+
     return 0;}
 double Haar(int k,int n,double t){
 if ((k-1)/pow(2,n)<=t && t<k/pow(2,n)){
