c3c3b7ae8fde8aca23d9d1061f7ff6944ed51d67
Kokoro <runnrairu@gmail.com>
2017-12-07 20:24:38 +0900

    Update Runge–Kutta.py

--- Runge–Kutta.py
+++ Runge–Kutta.py
@@ -1,30 +1,45 @@
+# -*- coding: utf-8 -*-
 import math
+import numpy as np
 import random
 def f(t,S):
-	return -G*G*S*S/(D*D)+2*F*S+C*C
+    t=float(t/n)
+    return -G*G*S*S/(D*D)+2*F*S+C*C
  
-n=1000
-X=[0]*(n+2)
-Y=[0]*(n+2)
-S=[0]*(n+2)
-Xhat=[0]*(n+2)
+n=1000 #時間の分割
+m=1000 #サンプルパスの数
+X=[0]*(n+1)
+Y=[0]*(n+1)
+S=[0]*(n+1)
+Xhat=[0]*(n+1)
+sum_loss=[0]*(n+1)
+moment=[[0 for i in range(n)] for j in range(m)]
+Var_loss=[0]*(n+1)
 delta_t=1.0/n
-F=0.1
-G=0.2
-C=0.2
-D=0.1
+double_loss=[0]*(n+1)
+F=1.0
+G=2.0
+C=2.0
+D=10.0
+sigma=np.power(delta_t,0.5)
+S_delta=1.0/n
 for i in range(n):
 	k1=f(i,S[i])
-	k2=f(i+delta_t/2,S[i]+delta_t*k1/2)
-	k3=f(i+delta_t/2,S[i]+delta_t*k2/2)
-	k4=f(i+delta_t,S[i]+delta_t*k3)
-	S[i+1]=S[i]+delta_t*(k1+2*k2+2*k3+k4)/6
- 
+	k2=f(i+S_delta/2,S[i]+S_delta*k1/2)
+	k3=f(i+S_delta/2,S[i]+S_delta*k2/2)
+	k4=f(i+S_delta,S[i]+S_delta*k3)
+	S[i+1]=S[i]+S_delta*(k1+2*k2+2*k3+k4)/6
+for j in range(m):
+    for i in range(n):
+        X[i+1]=F*X[i]*delta_t+C*np.random.normal(0,delta_t)
+        Y[i+1]=G*X[i]*delta_t+D*np.random.normal(0,delta_t)
+        Xhat[i+1]=(F-(G*G*S[i]/(D*D)))*Xhat[i]*delta_t+G*S[i]/(D*D)*(Y[i+1]-Y[i])
+        moment[j][i]=X[i]-Xhat[i]
+
 for i in range(n):
-	X[i+1]=F*X[i]*delta_t+C*random.gauss(0,delta_t)
-	Y[i+1]=G*X[i]*delta_t+D*random.gauss(0,delta_t)
-for i in range(n):
-	Xhat[i+1]=(F-(G*G*S[i]/(D*D)))*Xhat[i]*delta_t+G*S[i]/(D*D)*(Y[i+1]-Y[i])
-print (X)
-print ("こっからXhat")
-print (Xhat)
+    loss=0
+    for j in range(m):
+        loss=loss+np.power(moment[j][i],2)
+    Var_loss[i]=loss/m
+    double_loss[i]=np.absolute(S[i]-Var_loss[i])
+print(double_loss)
